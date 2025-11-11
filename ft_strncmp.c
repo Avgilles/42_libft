ee3704b6eb9e9e01afd1c1a949f4e49295dcfceb
Gilles Avraam <gavraam@f1r5s11.clusters.42paris.fr>
2025-11-11 13:50:15 +0100

    fix ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gavraam <gavraam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 15:18:24 by gavraam           #+#    #+#             */
-/*   Updated: 2025/11/09 10:37:18 by gavraam          ###   ########.fr       */
+/*   Updated: 2025/11/11 13:49:20 by gavraam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (s1[i] && (s1[i] == s2[i]) && i < (n -1))
+	while (i < n)
 	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (s1[i] == '\0')
+			return (0);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
+
 /*
 #include <stdio.h>
 #include <string.h>
